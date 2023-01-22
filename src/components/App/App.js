29f916872f8b8763f310d185a19e17af83fa1761
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-22 15:41:12 +0300

    сделал чекбокс

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,19 @@ import './App.css';
 //import Login from '../auth/Login/Login';
 //import Register from '../auth/Register/Register';
 import Header from '../common/Header/Header';
-import Profile from '../auth/Profile/Profile';
+//import Profile from '../auth/Profile/Profile';
 //import Main from '../Main/Main';
 //import Footer from '../common/Footer/Footer';
 //import SearchForm from '../Movies/SearchForm/SearchForm';
-import PageNotFound from '../PageNotFound/PageNotFound';
+//import PageNotFound from '../PageNotFound/PageNotFound';
+import Movies from '../Movies/Movies';
 
 
 function App() {
   return (
     <>
-      <PageNotFound/>
+    <Header/>
+      <Movies/>
     </>
   );
 }
--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./SearchForm.css"
 
-function SearchForm(){
-    return(
-     <section className="search-form">
 
-     </section>
+function SearchForm() {
+    return (
+        <section className="search-form">
+            <div className="search-form__container">
+                <form className="search-form__forma">
+                    <input
+                        className="section-form__input"
+                        id="text-input"
+                        required
+                        placeholder="Фильмы"
+                        type="text"
+                        name="text-link"
+                    />
+                    <button className="section-form__button"></button>
+                    <div className="checbox">
+                        <input className="checkbox__input" type="checkbox" id = "checkbox" />
+                        <label className="checkbox__label" for="checkbox" >Короткометражка</label>
+                    </div>
+
+                </form>
+            </div>
+        </section>
     );
 };
 
