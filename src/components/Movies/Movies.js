bbe0df80c9fcc0c3be9d53670c404aa1c2a8ab8a
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-27 15:13:51 +0300

    перед первым ревью

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,13 +2,15 @@ import React from "react";
 import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 import Footer from "../common/Footer/Footer";
+import HeaderProfile from '../common/Header/HeaderProfile.js'
 
 function Movies() {
     return (
         <>
+            <HeaderProfile />
             <SearchForm />
-            <MoviesCardList/>
-            <Footer/>
+            <MoviesCardList />
+            <Footer />
         </>
     )
 };
--- src/components/common/Header/HeaderProfile.js
+++ src/components/common/Header/HeaderProfile.js
@@ -29,12 +29,12 @@ function HeaderProfile(props) {
               <button className="menu-right__close"
                 onClick={handleChange}></button>
               <nav className="menu-right__navigetion">
-                <NavLink className="menu-right__item" activeClassName="menu__item_active" to="/tips">Главная</NavLink>
-                <NavLink className="menu-right__item" activeClassName="menu__item_active" exact to="/">Фильмы</NavLink>
-                <NavLink className="menu-right__item" activeClassName="menu__item_active" to="/tips">Сохраненные фильмы</NavLink>
+                <NavLink className="menu-right__item" activeClassName="menu__item_active" to="/">Главная</NavLink>
+                <NavLink className="menu-right__item" activeClassName="menu__item_active" exact to="/movies">Фильмы</NavLink>
+                <NavLink className="menu-right__item" activeClassName="menu__item_active" to="/saved-movies">Сохраненные фильмы</NavLink>
               </nav>
               <nav className="menu-right__accounts">
-                <NavLink className="menu-right__account" activeClassName="menu__item_active" to="/tips">Аккаунт</NavLink>
+                <NavLink className="menu-right__account" activeClassName="menu__item_active" to="/profile">Аккаунт</NavLink>
               </nav>
             </div>
           </section>
