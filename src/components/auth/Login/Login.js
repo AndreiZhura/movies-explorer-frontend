5dbf21d7048e3a0c4a74f03918d3fea1b2b47bda
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-29 16:22:39 +0300

    fix :1

--- src/components/auth/Login/Login.js
+++ src/components/auth/Login/Login.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 
 function Login(props) {
   return (
-    <section className="login">
+    <section className="login ">
       <div className="login-main">
-      <Link to="/" className="logo"></Link>
+      <Link to="/" className="logo logo_login"></Link>
       <div className="auth">
       <h1 className="auth__title auth__title_login">Рады видеть!</h1>
       <div className="auth-main">
@@ -34,7 +34,7 @@ function Login(props) {
         />
         <button className='auth-main__button auth-main__button_login'>Войти</button>
         <p className='auth-main__text auth-main__text_login'>Ещё не зарегистрированы?
-          <Link to="/signup" className='auth-main__text-button  '>Регистрация
+          <Link to="/signup" className='auth-main__text-button  auth-main__text-button_login '>Регистрация
           </Link></p>
       </form>
       </div>
