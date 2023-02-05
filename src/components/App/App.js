a9d3a2a8f55f8c973f75be4e8be97b2868d15b8a
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-02-05 18:54:01 +0300

    protectedRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
-
 import './App.css';
+import { useState, useEffect } from 'react';
 import Login from '../auth/Login/Login';
 import Register from '../auth/Register/Register';
 import Profile from '../auth/Profile/Profile';
@@ -7,97 +7,27 @@ import Main from '../Main/Main';
 import PageNotFound from '../PageNotFound/PageNotFound';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import { Navigate } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
-import { Route} from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import api from '../../utils/MainApi';
-import * as auth from "../../utils/auth";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
-import ProtectedRoute from "./ProtectedRoute";
+import { Route } from 'react-router-dom';
 
 
 function App() {
 
-
-  const [userData, setUserData] = useState({});
-  const [loggedIn, setloggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState("");
-  const [cards, setCards] = useState([]);
-  const [currentUser, setCurrentUser] = React.useState({});
-  // проверяем авторизован пользователь или нет
-  const [isLoggedIn, setisLoggedIn] = useState(false);
- 
-
-
-
-  const newAuth = (token) => {
-
-    return auth
-      .checkToken(token)
-      .then((res) => {
-        if (res) {
-          setisLoggedIn(true);
-          setloggedIn(true);
-          setUserEmail(res.data.email);
-          setUserData({
-            username: res.username,
-            email: res.email,
-          });
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  useEffect(() => {
-    if (!isLoggedIn) return;
-    api
-      .downLoadingUserInformationFromServer()
-      .then((res) => {
-        setCurrentUser(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-    api
-      .downloadingCardsFromServer()
-      .then((result) => {
-        setCards(result.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-  }, [isLoggedIn]);
+  const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <>
-     <CurrentUserContext.Provider value={currentUser}>
       <Routes>
-        <ProtectedRoute
-          path="/profile"
-          loggedIn={loggedIn}
-          component={Profile}
-        />
-         <ProtectedRoute
-          path="/movies"
-          loggedIn={loggedIn}
-          component={Movies}
-        />
-         <ProtectedRoute
-          path="/saved-movies"
-          loggedIn={loggedIn}
-          component={SavedMovies}
-        />
-      </Routes>
+      <Route path="/" element={loggedIn ? <Navigate to="/profile" replace /> : <Navigate to="/signin" replace />} /> 
         <Route path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Register />} />
         <Route path="*" element={<PageNotFound />} />
-
-        </CurrentUserContext.Provider>
+      </Routes>
     </>
   );
 }
