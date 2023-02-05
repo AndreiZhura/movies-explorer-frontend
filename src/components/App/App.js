2d7ff0c7bda2e2c51a1326e9e6ae3c5fea472b43
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-02-05 16:43:50 +0300

    fix start

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,21 +8,96 @@ import PageNotFound from '../PageNotFound/PageNotFound';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Route} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import api from '../../utils/MainApi';
+import * as auth from "../../utils/auth";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import ProtectedRoute from "./ProtectedRoute";
 
 
 function App() {
+
+
+  const [userData, setUserData] = useState({});
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [cards, setCards] = useState([]);
+  const [currentUser, setCurrentUser] = React.useState({});
+  // проверяем авторизован пользователь или нет
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+ 
+
+
+
+  const newAuth = (token) => {
+
+    return auth
+      .checkToken(token)
+      .then((res) => {
+        if (res) {
+          setisLoggedIn(true);
+          setloggedIn(true);
+          setUserEmail(res.data.email);
+          setUserData({
+            username: res.username,
+            email: res.email,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+    api
+      .downLoadingUserInformationFromServer()
+      .then((res) => {
+        setCurrentUser(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    api
+      .downloadingCardsFromServer()
+      .then((result) => {
+        setCards(result.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+  }, [isLoggedIn]);
+
   return (
     <>
+     <CurrentUserContext.Provider value={currentUser}>
       <Routes>
-      <Route path="/" element={<Main/>} />
-      <Route path="/movies" element={<Movies/>} />
-      <Route path="/saved-movies" element={<SavedMovies/>} />
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/signin" element={<Login/>} />
-      <Route path="/signup" element={<Register/>} />
-      <Route path="*" element={<PageNotFound/>} />
+        <ProtectedRoute
+          path="/profile"
+          loggedIn={loggedIn}
+          component={Profile}
+        />
+         <ProtectedRoute
+          path="/movies"
+          loggedIn={loggedIn}
+          component={Movies}
+        />
+         <ProtectedRoute
+          path="/saved-movies"
+          loggedIn={loggedIn}
+          component={SavedMovies}
+        />
       </Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="*" element={<PageNotFound />} />
+
+        </CurrentUserContext.Provider>
     </>
   );
 }
---
+++ src/utils/auth.js
@@ -0,0 +1,60 @@
+export const BASE_URL = 'http://api.andreizhura-diplom.nomoredomains.club';
+
+function getResponse(res) {
+  if(res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка ${res.status}`);
+}
+
+export const register = (email,password,name) => {
+
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password , name})
+  })
+
+  .then((res)=>{
+    return getResponse(res)
+  })
+};
+
+
+
+
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, password})
+  })
+  .then((res)=>{
+    return getResponse(res)
+  })
+};
+
+
+
+
+export const checkToken = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+  })
+  .then((res)=>{
+    return getResponse(res)
+  })
+} 
+
+
