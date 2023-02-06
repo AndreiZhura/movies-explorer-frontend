4717373d1e1c2e5f5612b53bcd2b360af57a178e
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-02-06 14:04:18 +0300

    fix:Registration

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,13 +11,44 @@ import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import ProtectedRoute from '../ProtectedRoute';
 import CurrentUserContext from '../../contexts/CurrentUserContext.js';
-import ApiMovies from '../../utils/MoviesApi'
+import ApiMovies from '../../utils/MoviesApi';
+import * as auth from "../../utils/auth";
 
 function App() {
 
   const [loggedIn, setloggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
   const [movies, setMovies] = useState([]);
+  const [userData, setUserData] = useState({});
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+
+
+  const newAuth = (token) => {
+    return auth
+      .checkToken(token)
+      .then((res) => {
+        if (res) {
+          setisLoggedIn(true);
+          setloggedIn(true);
+          setUserEmail(res.data.email);
+          setUserData({
+            username: res.username,
+            email: res.email,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      newAuth(token);
+    }
+  }, []);
 
   useEffect(() => {
     ApiMovies.AllMovies()
@@ -28,18 +59,21 @@ function App() {
       .catch((err) => {
         console.error(err);
       });
-  }, [])
+  }, []);
 
+
+  
   return (
     <>
       <CurrentUserContext.Provider value={currentUser}>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/movies" element={
-
-            <Movies
-              movies={movies}
-            />
+            <ProtectedRoute>
+              <Movies
+                movies={movies}
+              />
+            </ProtectedRoute>
 
           } />
           <Route path="/saved-movies" element={
@@ -53,7 +87,9 @@ function App() {
             </ProtectedRoute>
           } />
           <Route path="/signin" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
+          <Route path="/signup" element={<Register
+          
+          />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </CurrentUserContext.Provider>
