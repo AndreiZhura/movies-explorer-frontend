4433d2ebdec221e122d28084a68a65588d05ebcc
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-02-08 18:18:27 +0300

    fix: checkbox

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -7,7 +7,7 @@ import './MoviesCard.css'
 
 function MoviesCard(props) {
   return (
-    <a href={props.movie.trailerLink} target="_blank" className="movies">
+    <a href={props.movie.trailerLink} target="_blank" className="movies" rel="noreferrer">
       <h4 className="movies__title">{props.movie.nameRU}</h4>
       <p className="movies__time">{props.movie.duration} минут</p>
       <img src={`https://api.nomoreparties.co/${props.movie.image.formats.thumbnail.url}`} alt="фильм" className="movies__img" />
