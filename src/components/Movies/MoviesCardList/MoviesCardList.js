0e3b56ed847bbbe0173eace7f833a9fc75c908b3
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-02-11 18:45:15 +0300

    fix: ничего не найдено

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -5,15 +5,11 @@ import './MoviesCardList.css'
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({ movies }) {
-
-
-
-
-
+console.log(movies)
   return (
     <>
       <div className="movie-card-list">
-        {
+        { movies.length === 0 ? <div className="movie-card-list__error">Ничего не найдено</div>:
           movies.map((movie) => (
             <MoviesCard
               key={movie.id}
@@ -23,9 +19,12 @@ function MoviesCardList({ movies }) {
           ))
         }
       </div>
+      {
+        movies.length === 0 ? <></> :
       <div className="movies-buttons">
         <button className="movies-buttons__button">Ещё</button>
       </div>
+      }
     </>
   );
 };
